78d9a8768607826c5827fda8abdf93a1c9244b4b
kellyzhang1808 <kellyzhang1808@gmail.com>
2022-11-20 20:46:09 +0700

    upload

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,129 @@
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+  <!-- Add icons to the links using the .nav-icon class
+       with font-awesome or any other icon font library -->
+  
+       
+  <li class="nav-item"> 
+    <a href='{{ asset("/") }}' class="nav-link">
+      <i class="nav-icon fas fa-home"></i>
+      <p>
+        Home
+      </p>
+    </a>
+  </li>   
+  <li class="nav-item"> 
+    <a href='{{ asset("/admin/category") }}' class="nav-link">
+      <i class="nav-icon fas fa-th"></i>
+      <p>
+        Categories
+      </p>
+    </a>
+  </li>
+  <li class="nav-item"> 
+    <a href='{{ asset("/admin/user-role") }}' class="nav-link">
+      <i class="nav-icon fas fa-user"></i>
+      <p>
+        user Role
+      </p>
+    </a>
+  </li>
+  <li class="nav-item"> 
+    <a href='{{ asset("/admin/reservation-status") }}' class="nav-link">
+      <i class="nav-icon fas fa-th"></i>
+      <p>
+        Reservation Status
+      </p>
+    </a>
+  </li>
+  <li class="nav-item"> 
+    <a href='{{ asset("/admin/missing-item-status") }}' class="nav-link">
+      <i class="nav-icon fas fa-th"></i>
+      <p>
+        Missing Item Status
+      </p>
+    </a>
+  </li>
+  <li class="nav-item"> 
+    <a href='{{ asset("/admin/attendance") }}' class="nav-link">
+      <i class="nav-icon fas fa-hand"></i>
+      <p>
+        Attendance
+      </p>
+    </a>
+  </li>
+  <li class="nav-item"> 
+    <a href='{{ asset("/admin/reservation") }}' class="nav-link">
+      <i class="nav-icon fas fa-book"></i>
+      <p>
+        Reservation
+      </p>
+    </a>
+  </li>
+  <li class="nav-item"> 
+    <a href='{{ asset("/admin/schedule") }}' class="nav-link">
+      <i class="nav-icon fas fa-file"></i>
+      <p>
+        Schedule
+      </p>
+    </a>
+  </li>
+
+    {{-- <a href="/mahasiswa" class="nav-link">
+      <i class="nav-icon fas fa-th"></i>
+      <p>
+        Mahasiswa
+      </p>
+    </a>
+  </li>
+  {{-- <li class="nav-item">
+    <a href="/krs" class="nav-link">
+      <i class="nav-icon fas fa-th"></i>
+      <p>
+        krs
+      </p>
+    </a>
+  </li> --}}
+  {{-- <li class="nav-item">
+    <a href="/laporan/mahasiswa" class="nav-link">
+      <i class="nav-icon fas fa-th"></i>
+      <p>
+        Laporan Mahasiswa
+      </p>
+    </a>
+  </li> --}}
+  {{-- <li class="nav-item">
+    <a href="http://127.0.0.1:8000/admin/mahasiswa" class="nav-link">
+      <i class="nav-icon fas fa-th"></i>
+      <p>
+        Mahasiswa CRUD
+        <span class="right badge badge-danger">New</span>
+      </p>
+    </a>
+  </li> --}}
+  {{-- <li class="nav-item">
+    <a href="http://127.0.0.1:8000/admin/matakuliah" class="nav-link">
+      <i class="nav-icon fas fa-th"></i>
+      <p>
+        Matakuliah CRUD
+      </p>
+    </a>
+  </li> --}}
+  {{-- <li class="nav-item">
+    <a href="http://127.0.0.1:8000/admin/sertifikat" class="nav-link">
+      <i class="nav-icon fas fa-th"></i>
+      <p>
+        Sertifikat CRUD
+        <span class="right badge badge-danger">New</span>
+      </p>
+    </a>
+  </li> --}}
+  {{-- </li>
+  <li class="nav-item">
+    <a href="http://127.0.0.1:8000/admin/dosen" class="nav-link">
+      <i class="nav-icon fas fa-th"></i>
+      <p>
+        Dosen CRUD
+      </p>
+    </a>
+  </li> --}}
+
